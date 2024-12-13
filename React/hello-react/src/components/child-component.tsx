e530fa392869abe42aac465d1e03a9ab3458531f
Gyague Sonam <gyakhoe@gmail.com>
2024-12-13 13:39:36 -0500

    add two props and some notes

--- React/hello-react/src/components/child-component.tsx
+++ React/hello-react/src/components/child-component.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 
-// TODO: Notes on props
+/*
+ *
+ * In this component, props is of <ChildProps> type. What is that and what is props?
+ * Props is the data sent in from a parent component (The properties teh child component takes in)
+ * ParentComponent will render a ChildComponent in its TSX, and pass some values to it
+ * 
+ * We defined an interface (which is like a Java model) to define the values in this Child's props
+ * We can then use these values once they're sent in from a parent rendering this component
+ * 
+ */
 
-export const ChildComponent: React.FC<any> = ({ color }) => {
+interface ChildProps {
+    color: String
+    song: string
+}
+
+export const ChildComponent: React.FC<ChildProps> = ({ color, song }) => {
     return (
         <>
             <h4> Hello from Child Component </h4 >
             <p> My parent's fav color is : {color}</p>
+            <p> My parent's fav song is : {song}</p>
         </>
     );
 }
--- React/hello-react/src/components/parent-component.tsx
+++ React/hello-react/src/components/parent-component.tsx
@@ -2,13 +2,30 @@ import React, { useState } from "react";
 import { ChildComponent } from "./child-component";
 
 export const ParentComponent: React.FC = () => {
-    // TODO: Notes on state
-    const [favColor, setColor] = useState("Blue")
+
+    /*
+        *
+        * We can use the "useState" hook to define state 
+        * State is an object that stores data related to ca component
+        * 
+        * Why not just use a regular variable?
+        * There are two advantages to using state 
+        *   - We can easily pass state data to a child component's props
+        * When a component's state changes, it re-renders the component
+        * 
+        * To use state in a component, we declare:
+        *   1. A variable that lets us ACCESS the state value
+        *   2. A mutator (like a setter in Java) it lets us change the state value
+        *   3. The actual "useState" hook, which initializes state and lets us set default value
+        * 
+    */
+    const [favColor, setColor] = useState<string>("Blue");
+    const [favSong, setSong] = useState<string>("Give up the funk");
 
     return (
         <>
             <h3>Hello from the Parent Component</h3>
-            <ChildComponent color={favColor} />
+            <ChildComponent color={favColor} song={favSong} />
         </>
     );
 }
